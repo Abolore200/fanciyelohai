41330dbbb922a920452b19308cefe0c31fcd90bc
Your Name201~ <abolore200@gmail.com>
2023-04-17 19:09:12 +0100

    updated

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,4 @@
+//display the navigation bar, when the menu button is clicked
 function menuBar(){
     const navBar = document.querySelector('.navbar-list-link')
     navBar.classList.toggle('showNavbar')
@@ -7,3 +8,6 @@ function menuBar(){
         document.body.classList.remove('fixed')
     }
 }
+//insert the current year
+const year = new Date().getFullYear()
+document.querySelector('.copyright-year p span').innerHTML = year
