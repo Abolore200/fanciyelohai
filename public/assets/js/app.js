93cc7453079a335fc4742d243b480cab5f9f5d1a
Your Name201~ <abolore200@gmail.com>
2023-04-26 13:24:41 +0100

    updated the navigation link

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,6 +4,35 @@ function menuBar(){
     navBar.classList.toggle('showNavbar')
     if(navBar.classList.contains('showNavbar')){
         document.body.classList.toggle('fixed')
+
+        //array for all the navigation bar link [product,about us,contact us]
+        const click = document.querySelectorAll('.link')
+        const clickArray = Array.from(click)
+        //when the product link is clicked, navigate the page to product section
+        clickArray[0].addEventListener('click', () => {
+            if(document.body.classList.contains('fixed') && navBar.classList.contains('showNavbar')){
+                document.body.classList.remove('fixed')
+                navBar.classList.remove('showNavbar')
+            }
+            window.location.href = '#product'
+        })
+        //when the about us link is clicked, navigate the page to about us section
+        clickArray[1].addEventListener('click', () => {
+            if(document.body.classList.contains('fixed') && navBar.classList.contains('showNavbar')){
+                document.body.classList.remove('fixed')
+                navBar.classList.remove('showNavbar')
+            }
+            window.location.href = '#about-us'
+        })
+        //when the contact us link is clicked, navigate the page to contact us section
+        clickArray[2].addEventListener('click', () => {
+            if(document.body.classList.contains('fixed') && navBar.classList.contains('showNavbar')){
+                document.body.classList.remove('fixed')
+                navBar.classList.remove('showNavbar')
+            }
+            window.location.href = '#contact-us'
+        })
+        
     } else {
         document.body.classList.remove('fixed')
     }
